ec42555964f3595a72b467fa0fa3f57f6302fe58
Mohamed Osoble <ossoble360@gmail.com>
2023-11-07 11:45:52 -0500

    took forever because I tried to use innerText like in the example, but textcontent passed the test.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+function changeSentence() {
+    const targetElement = document.querySelector('#text');
+    targetElement.textContent = "This is really cool!";
+  }
+  
+  document.addEventListener("DOMContentLoaded", changeSentence);
